1a00eeea20f0ef780d51a113d41d7b4ce1dc1379
patdz <patdz@ymipro.com>
2021-12-24 11:30:24 +0800

    l

--- l/wrapper.go
+++ l/wrapper.go
@@ -1,25 +1,61 @@
 package l
 
-import "github.com/sgostarter/i/logger"
-
-func NewNopLoggerWrapper() logger.Wrapper {
-	return logger.NewNopLoggerWrapper()
-}
-
-func NewConsoleLoggerWrapper() logger.Wrapper {
-	return logger.NewConsoleLoggerWrapper()
-}
-
-func NewWrapper(l logger.Logger) logger.Wrapper {
-	return logger.NewWrapper(l)
-}
-
-func NewWrapperWithLevel(l logger.Logger, level logger.Level) logger.Wrapper {
-	if l == nil {
-		return nil
+func NewWrapper(l Logger) Wrapper {
+	return &wrapperImpl{
+		l: l,
 	}
+}
 
-	l.SetLevel(level)
+type wrapperImpl struct {
+	l Logger
+}
 
-	return logger.NewWrapper(l)
+func (wrapper *wrapperImpl) GetLogger() Logger {
+	return wrapper.l
+}
+
+func (wrapper *wrapperImpl) WithFields(fields ...Field) Wrapper {
+	l := wrapper.l.WithFields(fields...)
+
+	return NewWrapper(l)
+}
+
+func (wrapper *wrapperImpl) Fatal(a ...interface{}) {
+	wrapper.l.Log(LevelFatal, a...)
+}
+
+func (wrapper *wrapperImpl) Fatalf(format string, a ...interface{}) {
+	wrapper.l.Logf(LevelFatal, format, a...)
+}
+
+func (wrapper *wrapperImpl) Error(a ...interface{}) {
+	wrapper.l.Log(LevelError, a...)
+}
+
+func (wrapper *wrapperImpl) Errorf(format string, a ...interface{}) {
+	wrapper.l.Logf(LevelError, format, a...)
+}
+
+func (wrapper *wrapperImpl) Warn(a ...interface{}) {
+	wrapper.l.Log(LevelWarn, a...)
+}
+
+func (wrapper *wrapperImpl) Warnf(format string, a ...interface{}) {
+	wrapper.l.Logf(LevelWarn, format, a...)
+}
+
+func (wrapper *wrapperImpl) Info(a ...interface{}) {
+	wrapper.l.Log(LevelInfo, a...)
+}
+
+func (wrapper *wrapperImpl) Infof(format string, a ...interface{}) {
+	wrapper.l.Logf(LevelInfo, format, a...)
+}
+
+func (wrapper *wrapperImpl) Debug(a ...interface{}) {
+	wrapper.l.Log(LevelDebug, a...)
+}
+
+func (wrapper *wrapperImpl) Debugf(format string, a ...interface{}) {
+	wrapper.l.Logf(LevelDebug, format, a...)
 }
